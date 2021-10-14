8c41a3cf31101dc069885fe236c934ad5145999c
nedumd <davidsonchinedum@gmail.com>
2021-10-14 21:35:47 +0100

    react-ecommerce

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import NavBar from './components/NavBar';
+import HeroSection from './components/HeroSection';
+import Features from './components/Features';
+import Contents from './components/Contents'
+
+function App() {
+  return (
+    <div className="App">
+      <HeroSection />
+      <Features />
+      <Contents />
+    </div>
+  );
+}
+
+export default App;
