7a4712c4adc1a506018f224827c6a8be9248e32f
nedumd <davidsonchinedum@gmail.com>
2021-10-15 00:36:53 +0100

    react-ecommerce

--- src/components/Contents.js
+++ src/components/Contents.js
@@ -3,7 +3,7 @@ export default function Content() {
     <div>
       <div className="flex flex-col px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20 lg:flex-row">
         <div className="mb-5 lg:w-1/3 lg:mb-0 lg:mr-20">
-          <h2 className="relative mb-4 font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none">
+          <h2 className="relative mb-4 font-sans text-bold text-indigo-600 font-semibold tracking-wide uppercase">
             <span className="relative inline-block">
               <svg
                 viewBox="0 0 52 24"
@@ -27,11 +27,11 @@ export default function Content() {
                   height="24"
                 />
               </svg>
-              <span className="relative">The</span>
+              <span className="relative">Category</span>
             </span>{' '}
-            brown fox
+            For Everyone
           </h2>
-          <p className="mb-4 text-gray-900 lg:mb-6">
+          <p className="mb-4 text-2xl leading-relaxed font-medium lg:mb-6">
             Sed ut perspiciatis unde omnis iste natus error sit voluptatem
             laudantium, totam rem aperiam, eaque ipsa quae explicabo.
           </p>
@@ -51,26 +51,6 @@ export default function Content() {
           </a>
         </div>
         <div className="flex-grow pt-1">
-          <div className="flex items-center mb-3">
-            <span className="font-bold tracking-wide text-gray-900">
-              Categories
-            </span>
-            <span className="ml-1">
-              <svg
-                className="w-5 h-5 mt-px text-deep-purple-accent-400"
-                stroke="currentColor"
-                viewBox="0 0 52 52"
-              >
-                <polygon
-                  strokeWidth="4"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  fill="none"
-                  points="29 13 14 29 25 29 23 39 38 23 27 23"
-                />
-              </svg>
-            </span>
-          </div>
           <div className="grid grid-cols-2 row-gap-6 sm:grid-cols-4">
             <ul className="space-y-2">
               <li>
